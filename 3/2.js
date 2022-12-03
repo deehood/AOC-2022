1b35838b43d27d4b0b86f3d1360ddd6d3d0ff909
deehood <dee.hood@gmail.com>
2022-12-03 15:38:02 +0000

    part2

--- 3/2.js
+++ 3/2.js
@@ -1,27 +1,33 @@
 import * as fs from "fs";
 const data = fs.readFileSync("data", "utf8").split("\n");
 
-const isMixed = (str) =>
-  [...str].reduce((isMixed, next) => (isMixed = next !== str[0] ? true : false), false);
+const isInAllGroups = (str) =>
+  str.includes("0") && str.includes("1") && str.includes("2") ? true : false;
 
-function check(str) {
+function check(group) {
   const map = {};
-  [...str].map((letter, index) => {
-    if (!map[letter]) map[letter] = "";
-    map[letter] += index < str.length / 2 ? "a" : "b";
+  group.map((str, indexString) => {
+    [...str].map((letter) => {
+      if (!map[letter]) map[letter] = "";
+      map[letter] += indexString;
+    });
   });
 
   return Object.entries(map).reduce((sum, current) => {
-    if (isMixed(current[1]))
-      sum +=
+    if (isInAllGroups(current[1])) {
+      sum =
         current[0].charCodeAt(0) > 96
           ? current[0].charCodeAt(0) - 96
           : current[0].charCodeAt(0) - 38;
-
+    }
     return sum;
   }, 0);
 }
 
-const result = data.reduce((sum, next) => sum + check(next), 0);
+const groups = data.length / 3;
+let result = 0;
+for (let i = 0; i < groups; i++) {
+  result += check([data[i * 3], data[i * 3 + 1], data[i * 3 + 2]]);
+}
 
 console.log(result);
