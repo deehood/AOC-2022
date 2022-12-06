ad3f224ba8c9f55afdfb180c4790f4740e72fa85
deehood <dee.hood@gmail.com>
2022-12-06 10:02:54 +0000

    2

--- 6/1.js
+++ 6/1.js
@@ -3,9 +3,9 @@ const data = fs.readFileSync("data", "utf8").split("");
 
 const isUnique = (arr) => arr.length === new Set(arr).size;
 
-const arr = data.slice(0, 4);
+const arr = data.slice(0, 14);
 
-let i = 4;
+let i = 14;
 for (; i < data.length; i++) {
   if (isUnique(arr)) break;
   arr.splice(0, 1);
