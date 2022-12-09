4a03720dab944a89d7479ed3b6b86e77218d2be5
deehood <dee.hood@gmail.com>
2022-12-09 00:11:55 +0000

    1

--- 8/1.js
+++ 8/1.js
@@ -2,8 +2,6 @@ import * as fs from "fs";
 const dataStrings = fs.readFileSync("data", "utf8").split("\n");
 const data = dataStrings.map((x) => x.split(""));
 
-let newData = [];
-
 function rot90() {
   var n = newData.length;
   for (let i = 0; i < n / 2; i++) {
@@ -33,24 +31,19 @@ const checkSide = (direction) => {
   }
 };
 
-newData = data.map((line, i1) =>
+let newData = data.map((line, i1) =>
   line.map((_, i2) => {
     return { pos: i1.toString() + i2.toString(), val: line[i2], visible: [] };
   })
 );
 
 checkSide("W");
-console.log("W", newData);
 rot90();
-
 checkSide("N");
-console.log("N", newData);
 rot90();
 checkSide("E");
-console.log("E", newData);
 rot90();
 checkSide("S");
-console.log("S", newData);
 
 const res = newData.reduce(
   (acc, line) =>
@@ -61,8 +54,6 @@ const res = newData.reduce(
     }, 0),
   0
 );
+
 const edge = data.length * 4 - 4;
 console.log("visible", res + edge);
-
-const total = data.length ** 2;
-console.log("total", total);
