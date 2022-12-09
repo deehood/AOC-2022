42de5401492ebfb5611f45aad9d9be06aadaf9ee
deehood <dee.hood@gmail.com>
2022-12-09 01:30:13 +0000

    filenames

--- 8/1.js
+++ 8/1.js
@@ -2,8 +2,10 @@ import * as fs from "fs";
 const dataStrings = fs.readFileSync("data", "utf8").split("\n");
 const data = dataStrings.map((x) => x.split(""));
 
+let newData = [];
+
 function rot90() {
-  var n = newData.length;
+  let n = newData.length;
   for (let i = 0; i < n / 2; i++) {
     for (let j = i; j < n - i - 1; j++) {
       var tmp = newData[i][j];
@@ -15,38 +17,52 @@ function rot90() {
   }
 }
 
-const checkSide = () => {
+const checkSide = (direction) => {
   let line = [];
+  let max = 0;
 
   for (let i = 1; i < newData.length - 1; i++) {
     line = newData[i];
-    for (let j = 1; j < line.length; j++) {
-      let k = 1;
-      for (; k + j <= line.length - 2; k++) {
-        if (parseInt(line[j].val) <= parseInt(line[j + k].val)) break;
+    max = parseInt(line[0].val);
+    for (let j = 1; j < line.length - 1; j++) {
+      if (parseInt(line[j].val) > max) {
+        newData[i][j].visible.push(direction);
+        max = newData[i][j].val;
       }
-      newData[i][j].trees.push(k);
     }
   }
 };
 
-let newData = data.map((line, i1) =>
+newData = data.map((line, i1) =>
   line.map((_, i2) => {
-    return { pos: i1.toString() + i2.toString(), val: line[i2], trees: [] };
+    return { pos: i1.toString() + i2.toString(), val: line[i2], visible: [] };
   })
 );
 
 checkSide("W");
+console.log("W", newData);
 rot90();
+
 checkSide("N");
+console.log("N", newData);
 rot90();
 checkSide("E");
+console.log("E", newData);
 rot90();
 checkSide("S");
+console.log("S", newData);
 
-const resArray = newData.map((line) =>
-  line.map((item) => item.trees.reduce((product, next) => product * parseInt(next), 1))
+const res = newData.reduce(
+  (acc, line) =>
+    acc +
+    line.reduce((sum, pos) => {
+      sum += pos.visible.length > 0 ? 1 : 0;
+      return sum;
+    }, 0),
+  0
 );
+const edge = data.length * 4 - 4;
+console.log("visible", res + edge);
 
-const res = Math.max(...resArray.map((line) => Math.max(...line)));
-console.log("res", res);
+const total = data.length ** 2;
+console.log("total", total);
