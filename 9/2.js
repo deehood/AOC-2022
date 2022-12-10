0f824971da45033940e066f01ed565cb5670e89f
deehood <dee.hood@gmail.com>
2022-12-10 13:30:43 +0000

    2

--- 9/2.js
+++ 9/2.js
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-const data = fs.readFileSync("data2", "utf8").split("\n");
+const data = fs.readFileSync("./data3", "utf8").split("\n");
 
 class Rope {
   constructor(name, x, y) {
@@ -26,63 +26,121 @@ class Rope {
         this.x -= 1;
         break;
       case "U":
-        this.y -= 1;
+        this.y += 1;
         break;
       case "D":
-        this.y += 1;
+        this.y -= 1;
         break;
     }
     this.history.push({ x: this.x, y: this.y });
   }
 }
 
-const isTouching = (h, t) => Math.abs(h.x - t.x) < 2 && Math.abs(h.y - t.y) < 2;
-
-const isSideways = (h, t) => Math.abs(h.x - t.x) + Math.abs(h.y - t.y) === 3;
+const distance = (h, t) => Math.abs(h.x - t.x) + Math.abs(h.y - t.y);
 
 const H = new Rope("H", 0, 0);
 const T = [];
 for (let i = 1; i < 10; i++) {
   T.push(new Rope(i.toString(), 0, 0));
 }
+
+const dots = (size) => {
+  let offset = size / 2;
+  let rows = [];
+  let dots = [];
+  let chr = ".";
+  for (let i = size - 1; i >= 0; i--) {
+    rows[i] = [];
+
+    for (let j = 0; j < size; j++) {
+      chr = ".";
+      if (H.getPos().x + offset == j && H.getPos().y + offset == i) chr = "H";
+      for (let k = 8; k >= 0; k--) {
+        if (T[k].getPos().x + offset == j && T[k].getPos().y + offset == i)
+          chr = (k + 1).toString();
+      }
+      rows[i].push(chr);
+    }
+    dots.push(rows[i]);
+  }
+  return dots;
+};
+
 let parent = [];
 let inst = "";
 let val = "";
+let deltaX,
+  deltaY = 0;
 for (let line of data) {
   [inst, val] = [...line.split(" ")];
 
   for (let i = 1; i <= parseInt(val); i++) {
     H.instruction(inst);
 
-    if (inst === "U") {
-      console.log(
-        T.map((el) => {
-          return { name: el.name, x: el.x, y: el.y };
-        })
-      );
-
-      console.log(inst);
-      console.log({ name: H.name, x: H.x, y: H.y });
-    }
-
     for (let j = 0; j < 9; j++) {
       parent = j > 0 ? T[j - 1] : H;
-      if (!isTouching(parent.getPos(), T[j].getPos())) {
-        isSideways(parent.getPos(), T[j].getPos())
-          ? T[j].setPos(
+
+      switch (distance(parent.getPos(), T[j].getPos())) {
+        case 2: {
+          // follow the parent
+          if (parent.x === T[j].x) T[j].setPos(T[j].x, (parent.y + T[j].y) / 2);
+          if (parent.y === T[j].y) T[j].setPos((parent.x + T[j].x) / 2, T[j].y);
+
+          break;
+        }
+        case 3: {
+          //move to old parent position if it makes it right next to parent
+          if (
+            distance(
+              {
+                x: parent.history[parent.history.length - 2].x,
+                y: parent.history[parent.history.length - 2].y,
+              },
+              parent.getPos()
+            ) === 1
+          ) {
+            T[j].setPos(
               parent.history[parent.history.length - 2].x,
               parent.history[parent.history.length - 2].y
-            )
-          : T[j].instruction(inst);
+            );
+          } else {
+            // move the same way the parent did
+            deltaX =
+              parent.history[parent.history.length - 1].x -
+              parent.history[parent.history.length - 2].x;
+            deltaY =
+              parent.history[parent.history.length - 1].y -
+              parent.history[parent.history.length - 2].y;
+            T[j].setPos(T[j].x + deltaX, T[j].y + deltaY);
+          }
+
+          break;
+        }
+
+        case 4: {
+          deltaX =
+            parent.history[parent.history.length - 1].x -
+            parent.history[parent.history.length - 2].x;
+          deltaY =
+            parent.history[parent.history.length - 1].y -
+            parent.history[parent.history.length - 2].y;
+          T[j].setPos(T[j].x + deltaX, T[j].y + deltaY);
+
+          break;
+        }
+        default:
+          //do nothing
+          break;
       }
     }
   }
+
+  console.log(inst, val);
+  console.table(dots(30));
 }
-// console.log("H", H);
-// console.log("T8", T[7]);
-// console.log("T9", T[8]);
-// console.log("T1", T[0]);
-// console.log("T2", T[1]);
+
+console.log("T9", T[8]);
 
 const result = new Set(T[8].history.map((x) => JSON.stringify(x)));
+
 console.log("visited", result.size);
