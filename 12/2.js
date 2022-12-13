27190d11b9bc92ee3cc5d3ab63569e24a8722bd2
deehood <dee.hood@gmail.com>
2022-12-13 14:30:32 +0000

    2

--- 12/2.js
+++ 12/2.js
@@ -5,8 +5,8 @@ let data = dataLines.map((line) => [...line]);
 let S = {};
 let A = [];
 let results = [];
-let ROWS = data.length;
-let COLS = data[0].length;
+let maxRows = data.length;
+let maxCols = data[0].length;
 
 data.forEach((line, index1) =>
   line.map((chr, index2) => {
@@ -28,12 +28,12 @@ const adjacency = (current) => {
   let currentChr = data[row][col].charCodeAt(0);
   if (row > 0) if (data[row - 1][col].charCodeAt(0) - currentChr < 2) arr.push([row - 1, col]);
 
-  if (row < ROWS - 1)
+  if (row < maxRows - 1)
     if (data[row + 1][col].charCodeAt(0) - currentChr < 2) arr.push([row + 1, col]);
 
   if (col > 0) if (data[row][col - 1].charCodeAt(0) - currentChr < 2) arr.push([row, col - 1]);
 
-  if (col < COLS - 1)
+  if (col < maxCols - 1)
     if (data[row][col + 1].charCodeAt(0) - currentChr < 2) arr.push([row, col + 1]);
 
   return arr;
@@ -51,7 +51,7 @@ for (let i = 0; i < A.length; i++) {
   while (queue.length > 0) {
     current = queue.shift();
     if (data[current[0]][current[1]] === "z") {
-      console.log("hey", data[current[0]][current[1]]);
+      console.log("->", data[current[0]][current[1]]);
       console.log(current);
       results.push(current[2]);
       break;
